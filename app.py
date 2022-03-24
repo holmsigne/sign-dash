0004979665fe07dd41e91333a3476da8fd98d4da
holmsigne <holm.signe@hotmail.com>
2022-03-24 09:26:52 +0100

    test

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ fig_employee = px.bar(order,
     text='total', title='Sales by Employee',
     hover_data=[],
     labels={'total':'Total sales', 'employee':'Employee', 'type':'Product Type'})
-fig_employee.update_traces(texttemplate='%{text:.5s}', textposition='outside')
+fig_employee.update_traces(texttemplate='%{text:0.2s}', textposition='outside')
 fig_employee.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
 
 fig_product = px.bar(order, 
@@ -42,7 +42,7 @@ fig_product = px.bar(order,
     text='total', title='Sales by Product',
     hover_data=[],
     labels={'total':'Total sales', 'productname':'Product', 'type':'Product Type'})
-fig_product.update_traces(texttemplate='%{text:.5s}', textposition='outside')
+fig_product.update_traces(texttemplate='%{text:0.2s}', textposition='outside')
 fig_product.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
 
 # ***************************************
@@ -60,30 +60,26 @@ dash_app.layout = html.Div(
     children=[
         html.Div(className='row',
                 children=[
-                    #html.Div(className='four columns div-user-controls',
-                    #        children=[
-                    #            html.H2('Sales dashboard'),
-                    #            html.P('Select filters from dropdown'),
-
-                    #html.Div(children="Month", className="menu-title"),
-                    #        dcc.Dropdown(
-                    #            id='drop_month',
-                    #            options=[{'label':selectmonth, 'value':selectmonth} for selectmonth in df_month['monthnames']],
-                    #        ),
-                    #html.Div(children="Year", className="menu-title"),
-                    #        dcc.Dropdown(
-                    #            id='drop_year',
-                    #            options=[{'label':selectyear, 'value':selectyear} for selectyear in df_year]
-                    #        ),
-                    #        ]
-                    #),
-                    html.Div(className='eight columns div-for-charts bg-grey',
+                    html.Div(className='four columns div-user-controls',
                             children=[
-                                dcc.Graph(id="sales_employee", figure=fig_employee)
+                                html.H2('Sales dashboard'),
+                                html.P('Select filters from dropdown'),
+
+                    html.Div(children="Month", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_month',
+                                options=[{'label':selectmonth, 'value':selectmonth} for selectmonth in df_month['monthnames']],
+                            ),
+                    html.Div(children="Year", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_year',
+                                options=[{'label':selectyear, 'value':selectyear} for selectyear in df_year]
+                            ),
                             ]
                     ),
                     html.Div(className='eight columns div-for-charts bg-grey',
                             children=[
+                                dcc.Graph(id="sales_employee", figure=fig_employee),
                                 dcc.Graph(id="sales_product", figure=fig_product)
                             ]
                     ),
@@ -97,32 +93,32 @@ dash_app.layout = html.Div(
 # ***************************************
 # Output er diagrammet
 # Input er DropDown
-#@dash_app.callback(Output('sales_employee', 'figure'),
-#              [Input('drop_month', 'value')],
-#              [Input('drop_year', 'value')])
+@dash_app.callback(Output('sales_employee', 'figure'),
+              [Input('drop_month', 'value')],
+              [Input('drop_year', 'value')])
 
-#def update_graph(drop_month, drop_year):
-#    if drop_year:
-#        if drop_month:
+def update_graph(drop_month, drop_year):
+    if drop_year:
+        if drop_month:
             # Data i både drop_month og drop_year
-#            order_fig1 = order.loc[(order['orderyear'] == drop_year) & (order['ordermonth'] == drop_month)]
-#        else:
+            order_fig1 = order.loc[(order['orderyear'] == drop_year) & (order['ordermonth'] == drop_month)]
+        else:
             # Data i drop_year. men ikke drop_month
-#            order_fig1 = order.loc[order['orderyear'] == drop_year]
-#    else:
-#        if drop_month:
+            order_fig1 = order.loc[order['orderyear'] == drop_year]
+    else:
+        if drop_month:
             # Data i drop_month, men ikke drop_year
-#            order_fig1 = order.loc[order['ordermonth'] == drop_month]
-#        else:
+            order_fig1 = order.loc[order['ordermonth'] == drop_month]
+        else:
             # Ingen data - ikke noget valgt
-#            order_fig1 = order
+            order_fig1 = order
         
-#    return {'data':[go.Bar(
-#        x = order_fig1['productname'],
-#        y = order_fig1['total']
-#            )
-#        ]
-#    }
+    return {'data':[go.Bar(
+        x = order_fig1['productname'],
+        y = order_fig1['total']
+            )
+        ]
+    }
 
 # ***************************************
 # Run the app
